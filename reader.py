7bc08464c3ad897178616c257c0ead329339b0e0
Kyle <kyle.trng@gmail.com>
2024-05-11 23:57:22 -0400

    Remove extra characters from OCR

--- reader.py
+++ reader.py
@@ -7,7 +7,7 @@ for i in range(len(images)):
     with open("page"+str(i)+".jpg", "r") as file:
         table = image_to_string("page"+str(i)+".jpg", config="--psm 6")
         # Remove title from each page
-        table = table.replace("Record of employees' 2023 salaries and benefits", "")
-        table = table.replace("Registre des traitements et avantages verses aux employes en 2023", "")
+        table = table.replace("Record of employees' 2023 salaries and benefits", "").replace("Registre des traitements et avantages verses aux employes en 2023", "")
+        table = table.replace("~","").replace("|", "").replace("'","").replace("(","").replace("_","").replace("=","").replace("-","")
         print(table)
     remove("page"+str(i)+".jpg")
