e136e893ab55d0caee43a09906738ea0d3af9c95
Kyle <kyle.trng@gmail.com>
2024-05-15 03:04:03 -0400

    Fix csv, create dataframe

--- data.py
+++ data.py
@@ -1,9 +1,12 @@
 from reader import load_data
 from os import remove, path
 import pandas as pd
+import numpy as np
 
 if not(path.isfile("output.csv")):
     load_data()
 
-df = pd.read_csv("output.csv", header=None, names=['Calendar Year', 'Sector', 'Last Name', 'First Name', 'Position Title', 'Organization', 'Salary Paid', 'Taxable Benefits'])
-df
+df = pd.read_csv("output.csv", header=None, names=['Last Name', 'First Name', 'Position Title', 'Salary Paid', 'Taxable Benefits'], na_values='-', on_bad_lines='warn')
+df['Salary Paid'] = df['Salary Paid'].str.replace(",","").astype(float)
+df['Taxable Benefits'] = df['Taxable Benefits'].str.replace(" ", "").str.replace(",","").astype(float)
+print(df)
--- reader.py
+++ reader.py
@@ -2,11 +2,12 @@ from tabula import convert_into
 from os import remove
 
 def load_data():
-    convert_into("2023-PSSD-York-University.pdf", "infile.csv", output_format="csv", pages='all')
+    convert_into("2023-PSSD-York-University.pdf", "infile.csv", output_format="csv", guess=False, pages="all")
     print("Done conversion!")
     with open("infile.csv") as filein, open("output.csv", "w") as fileout:
             for line in filein:
                 if line.split(",")[0] == "2023":
+                    line = line.replace("2023,Universities,","").replace("York University","").replace(",,",",")
                     fileout.write(line)
                 else:
                     continue
