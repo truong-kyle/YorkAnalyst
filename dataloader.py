3b722413433992ef2ac8af410e3512791b9895b0
Kyle Truong <kyle.trng@gmail.com>
2024-05-15 10:01:07 -0400

    Fix: Taxable Benefits column

--- dataloader.py
+++ dataloader.py
@@ -5,5 +5,5 @@ import re
 url = "https://raw.githubusercontent.com/truong-kyle/YorkAnalyst/main/data/2023-PSSD-York-University-output.csv"
 df = pd.read_csv(url, header=None, names=['Last Name', 'First Name', 'Position Title', 'Salary Paid', 'Taxable Benefits'], na_values='-', on_bad_lines='warn')
 df['Salary Paid'] = df['Salary Paid'].astype(float)
-df['Taxable Benefits'] = df['Taxable Benefits'].str.replace(" ", "").str.replace(",","").astype(float)
+df['Taxable Benefits'] = df['Taxable Benefits'].astype(float)
 print(df)
