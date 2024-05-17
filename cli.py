2014964be6982967454c9f9e9b01a2cb44fcadb4
Kyle Truong <kyle.trng@gmail.com>
2024-05-16 22:22:27 -0400

    Add: cli commands, full name search

--- cli.py
+++ cli.py
@@ -5,6 +5,27 @@ from rich import print
 
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
+
+def search_name(df, name):
+    #input is a dataframe and name, but name can be first name, last name or both. need to match specific cases
+    #return the row that matches the name
+    #if no match, return None
+    name = name.lower()
+    name = name.split(" ")
+    if len(name) == 1:
+        #search for first name or last name
+        result = df[(df['First Name'].str.contains(name[0], case=False)) | (df['Last Name'].str.contains(name[0], case=False))]
+    else:
+        #search for both first name and last name
+        result = df[(df['First Name'].str.contains(name[0], case=False)) & (df['Last Name'].str.contains(name[1], case=False))]
+    if result.empty:
+        return None
+    else:
+        return result
+
+
+print("Loading data...")
+
 offline = "data/2023-PSSD-York-University-output.csv"
 url = "https://raw.githubusercontent.com/truong-kyle/YorkAnalyst/main/data/2023-PSSD-York-University-output.csv"
 try: df = dataloader.load_data(url)
@@ -16,7 +37,7 @@ time.sleep(1)
 cls()
 time.sleep(0.5)
 while True:
-    print("[red]York University Salary Data: 2023")
+    print("York University Salary Data: 2023")
     print("==================================")
     print("1. Display the first X rows")
     print("2. Search")
@@ -28,8 +49,9 @@ while True:
         input("Press Enter to continue...")
         cls()
     elif choice == "2":
-        search = input("Enter search term: ")
-        print(df[df['Position Title'].str.contains(search, case=False)])
+        result = input("Enter name to search: ")
+        print(search_name(df, result))
+        input("Press Enter to continue...")
     elif choice == "3":
         exit()
     else:
