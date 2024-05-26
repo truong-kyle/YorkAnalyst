4fd75898011f89bbd0c0ec258b51a5f7705a5f6d
Kyle Truong <kyle.trng@gmail.com>
2024-05-26 17:46:36 -0400

    Add: table loading

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, request
+import search
+from load import load_data
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 
 def index():
+    if request.method == 'POST':
+        first = request.form['fname']
+        search.first(first, df)
     return render_template('index.html')
 
 if __name__ == "__main__":
