872ca928abbd21a3a2578b07a6ebbb1b06d7776b
Kyle Truong <kyle.trng@gmail.com>
2024-06-17 11:49:13 -0400

    Update: 2022 Salary Database

--- config/build.py
+++ config/build.py
@@ -1,5 +1,5 @@
 import sqlite3
-from config.dataloader import load_data
+from dataloader import load_data
 ## Rebuild the databases from the contents of the CSV files
 def build_db(year):
     url = f"https://raw.githubusercontent.com/truong-kyle/YorkAnalyst/main/data/20{year}-PSSD-York-University-output.csv"
@@ -8,4 +8,4 @@ def build_db(year):
     dataframe.to_sql('salaries'+str(year), sqlite3.connect(f'salaries{year}.db'), if_exists='replace', index=False)
 
 if __name__ == "__main__":
-    build_db(23)
+    build_db(22)
