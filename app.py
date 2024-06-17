adba2d0b27a92414f7128b178dfdd4c574112aa8
Kyle Truong <kyle.trng@gmail.com>
2024-06-17 12:05:52 -0400

    Fix issue with year value not being set in form submission

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 
 def index():
-    year = 23
+    
     
     if request.method == 'POST':
-        year = request.form['year']
+        year = request.form['year'] if request.form.get('year') else 23
         lname = request.form['lname']
         fname = request.form['fname']
         title = request.form['position']
@@ -16,6 +16,7 @@ def index():
         max_sal = request.form['maxsal']
         data = get_table_by_search(year, lname, fname, title, min_sal, max_sal)
     else:
+        year = 23
         data = get_table(year)
 
     count = len(data)
