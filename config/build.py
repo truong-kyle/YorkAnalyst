c6b4d1e71fade07e3677dd15c907cfbb6631552f
Kyle Truong <kyle.trng@gmail.com>
2024-06-17 13:13:26 -0400

    chore: Update build script to save salaries database in the 'db' directory

--- config/build.py
+++ config/build.py
@@ -5,7 +5,7 @@ def build_db(year):
     url = f"https://raw.githubusercontent.com/truong-kyle/YorkAnalyst/main/data/20{year}-PSSD-York-University-output.csv"
     dataframe = load_data(url)
     print(dataframe)
-    dataframe.to_sql('salaries', sqlite3.connect(f'salaries{year}.db'), if_exists='replace', index=False)
+    dataframe.to_sql('salaries', sqlite3.connect(f'db/salaries{year}.db'), if_exists='replace', index=False)
 
 if __name__ == "__main__":
-    build_db(22)
+    build_db(23)
