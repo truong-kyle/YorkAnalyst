bba477a76176691b00025d28bda7243194477390
Kyle Truong <kyle.trng@gmail.com>
2024-06-16 20:39:57 -0400

    Add: last comma deletes

--- config/regexclean.py
+++ config/regexclean.py
@@ -1,7 +1,7 @@
 import re
 def fix_salary(path:str):
     with open(path, "r") as filein, open(f'{path}.output', "w") as fileout:
-        salary_pattern = r'\"(\d{1,3}(,\d{3})*\.\d*)\"'
+        salary_pattern = r'\"(\d{1,3}(,\d{1,3})*\.\d*)\"'
         for line in filein:
             salary_match = re.search(salary_pattern, line)
             if salary_match:
@@ -11,5 +11,13 @@ def fix_salary(path:str):
                 line = re.sub(salary_pattern, salary, line)
             fileout.write(line)
 
+def del_last_commas(path:str):
+    with open(path, "r") as filein, open(f'{path}.output', "w") as fileout:
+        for line in filein:
+            linelist = line.split()
+            if linelist[-1][-1] == ',':
+                linelist[-1] = linelist[-1][:-1]
+            fileout.write(" ".join(linelist) + '\n')
+
 if __name__ == "__main__":
-    fix_salary("data/2023-PSSD-York-University.csv")
+    fix_salary("config/2021-PSSD-York-University.csv")
