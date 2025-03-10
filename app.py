b9b94aa392f7129ab6216d8d1387924e3c1d918f
Kyle Truong <kyle.trng@gmail.com>
2025-03-10 12:36:07 -0400

    Flip first and last names

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template, request
 from stroke import get_table, get_table_by_search
 
 app = Flask(__name__)
-@app.route('/', methods=['GET', 'POST'])
 
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    
-    
+    # Initialize year variable
+    year = 23  # Default year
+
     if request.method == 'POST':
-        year = request.form['year'] if request.form.get('year') else 23
+        # Update year based on form input
+        year = request.form['year'] if request.form.get('year') else year
         lname = request.form['lname']
         fname = request.form['fname']
         title = request.form['position']
@@ -16,11 +18,11 @@ def index():
         max_sal = request.form['maxsal']
         data = get_table_by_search(year, lname, fname, title, min_sal, max_sal)
     else:
-        year = 23
+        # Fetch data for the default year
         data = get_table(year)
 
     count = len(data)
     return render_template('index.html', results=data, year=year, count=count)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
